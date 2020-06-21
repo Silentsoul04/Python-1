2c013ed19ffac857c40467f1bf1d3f600e44f9aa
Jason_glb <408qqmail@qq.com>
2020-06-21 21:20:24 +0800

    2020-0621-1stchange

---
+++ Database/day01/10.py
@@ -0,0 +1,30 @@
+class Person:
+
+    def __init__(self,name,weight):
+        self.name = name
+        self.weight = weight
+
+
+    def __str__(self):
+        return "我的名字是 %s 体重是 %.2f 公斤" % (self.name,self.weight)
+
+
+    def eat(self):
+        self.weight += 1
+        print("%s 是吃货，吃完这顿再减肥" % self.name)
+
+
+    def run(self):
+        self.weight -= 0.5
+        print("%s 爱跑步，跑步锻炼身体" % self.name)
+
+
+
+xiaoming = Person("小明",75.0)
+
+xiaoming.run()
+xiaoming.eat()
+
+print(xiaoming)
+
+# 
