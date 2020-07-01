66c21fe1fe50d89f25fe864a4e6a16fd1674989b
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-07-01 21:17:44 +0800

    update 14py

---
+++ Database/day01/14.py
@@ -0,0 +1,36 @@
+class Gun:
+
+    def __init__(self,model):
+
+        self.model = model
+        self.bullet_count = 0
+
+    def add_bullet(self,count):
+
+        self.bullet_count += count
+
+    def shoot(self):
+
+        # 1.判断子弹数量
+        if self.bullet_count <= 0:
+
+            print("[%s] 子弹不够了，快装弹" % self.model)
+            return
+
+        # 2.发射子弹，-1
+        self.bullet_count -= 1
+
+        # 3.提示发射信息
+        print("老子刚刚发射了一枚子弹，可还行")
+        print("[%s] 突突突... [%d]" % (self.model,self.bullet_count))
+
+
+
+ak47 = Gun("AK47")
+
+ak47.add_bullet(50)
+
+ak47.shoot()
+
+
+
