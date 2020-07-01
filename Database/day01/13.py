8e31135b7ee6fea4297664b5b17fd6aa287430a4
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-07-01 09:28:02 +0800

    Update 13.py

--- Database/day01/13.py
+++ Database/day01/13.py
@@ -34,13 +34,22 @@ class House:
 
         print("要添加 %s" % item)
         # 1. 判断家具的面积
+        if item.area > self.free_area:
+            print("%s 的面积太大了，无法添加" % item.name)
 
-        # 2.
+            return
+
+        # 2.将家具的名称添加到列表中
+            self.item_list.append(item.name)
+
+        # 3.计算剩余面积
+        self.free_area -= item.area
+
 
 # 1.创建家具
-bed = HouseItem("席梦思",4)
+bed = HouseItem("席梦思",40)
 chest = HouseItem("衣柜",2)
-table = HouseItem("餐桌",1.5)
+table = HouseItem("餐桌",20)
 
 print(bed)
 print(chest)
@@ -48,10 +57,10 @@ print(table)
 
 
 # 2.创建房子对象
-my_home = House("别墅",60)
+my_home = House("两室一厅",60)
 
-# my_home.add_item(bed)
-# my_home.add_item(chest)
-# my_home.add_item(table)
+my_home.add_item(bed)
+my_home.add_item(chest)
+my_home.add_item(table)
 
 print(my_home)
