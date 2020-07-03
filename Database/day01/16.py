f76ffad8045e0114c49a553a5c28ec91a04c6bfa
Jason_glb <408qqmail@qq.com>
2020-07-03 23:19:18 +0800

    2020-07-03-first-change

--- Database/day01/16.py
+++ Database/day01/16.py
@@ -35,16 +35,32 @@ class Soldier:
         # 2.枪 - 新兵没有枪
         self.gun = None
 
+    def fire(self):
+
+        # 1.判断士兵是否有枪
+        if self.gun == None:
+            print("%s 没有枪，我要去拿枪" % self.name)
+            return
+
+        # 2.高喊口号
+        print("兄弟们，都是有枪的人，亮出家伙%s来" % self.gun)
+
+        # 3.让枪装填子弹
+        self.gun.add_bullet(20)
+        # 4.让枪发射子弹
+
 
 # 1. 创建枪对象
 
 ak47 = Gun("AK47")
 
-ak47.add_bullet(50)
-ak47.shoot()
 
 # 2.创建许三多
 xusanduo = Soldier("许三多")
+
+xusanduo.gun = ak47
+
+xusanduo.fire()
 
 print(xusanduo.gun)
 
