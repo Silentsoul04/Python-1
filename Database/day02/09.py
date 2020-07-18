19a69aa341bf0b5182fc6fc981a2fafb115c653a
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-07-18 17:52:28 +0800

    2020-0718-firstchange

---
+++ Database/day02/09.py
@@ -0,0 +1,19 @@
+class A:
+
+    def test(self):
+        print("That's test method")
+
+
+class B:
+
+    def demo(self):
+            print("That's demo method")
+
+
+class C(A,B):
+
+    pass
+
+c = C()
+c.test()
+c.demo()
