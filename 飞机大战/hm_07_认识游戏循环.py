7584d55bb19ecbbd76cf87c70c467195cade78e4
gettotheghighershouldbe <57565161+gettotheghighershouldbe@users.noreply.github.com>
2020-10-19 09:39:46 +0800

    2020-10-19firstchange

---
+++ 飞机大战/hm_07_认识游戏循环.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+# 1. 创建游戏的窗口 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+# 2. 绘制背景图像
+bg = pygame.image.load("./Image/background.png")
+screen.blit(bg, (0, 0))
+# pygame.display.update()
+
+# 3.绘制英雄的飞机
+hero = pygame.image.load("./Image/me1.png")
+screen.blit(hero, (150, 300))
+
+# 可以在所有绘制工作完成后，统一调用update方法
+pygame.display.update()
+
+
+# 游戏循环 -> 意味着游戏的正式开始
+while True:
+
+    pygame.event.get(1000)
+
+
+pygame.quit()
